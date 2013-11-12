50c4c5265150a9763681e0a780498fbafda8ec57
Christian Nilsson <christian@klamby.com>
2013-11-12 15:35:48 +0100

    Class skeleton

---
+++ NumberGuessingGame/Models/SecretNumber.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NumberGuessingGame.Models
+{
+    public class SecretNumber
+    {
+        private int? _number;
+        private int MaxNumberOfGuesses;
+
+        public bool CanMakeGuess
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IList<GuessedNumber> GuessedNumbers
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public GuessedNumber LastGuessedNumber
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int? Number { get; set; }
+
+        public SecretNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Outcome MakeGuess(int guess)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
