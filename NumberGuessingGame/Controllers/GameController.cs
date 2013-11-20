cd020e925797ee8785cd9ebed203563256d44266
Christian Nilsson <christian@klamby.com>
2013-11-20 10:26:10 +0100

    Some views

---
+++ NumberGuessingGame/Controllers/GameController.cs
@@ -0,0 +1,50 @@
+﻿using NumberGuessingGame.Models;
+using NumberGuessingGame.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NumberGuessingGame.Controllers
+{
+    public class GameController : Controller
+    {
+        private Game game;
+
+        public GameController()
+        {
+            game = new Game();
+
+            try
+            {
+                game.SecretNumber = Session["SecretNumber"] as SecretNumber;
+            }
+            catch (Exception)
+            {
+                game.SecretNumber = new SecretNumber();
+            }
+        }
+
+        //
+        // GET: /Game/
+
+        public ActionResult Index()
+        {
+            return View(game);
+        }
+
+        [HttpPost]
+        public ActionResult Index(/*[Bind(Include="Guess")]*/Game guess)
+        {
+            var req = Request["Guess"];
+            //if (ModelState.IsValid)
+            //{
+                game.SecretNumber.MakeGuess(guess.Guess);
+                Session["SecretNumber"] = game.SecretNumber;
+            //}
+
+            return View(game);
+        }
+    }
+}
---
+++ NumberGuessingGame/ViewModels/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NumberGuessingGame.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Providers.Entities;
+
+namespace NumberGuessingGame.ViewModels
+{
+    public class Game
+    {
+        public SecretNumber SecretNumber { get; set; }
+
+        [Required]
+        public int Guess { get; set; }
+    }
+}
