8ce8bc95266da5f1e777f77737449152a990690c
Christian Nilsson <christian@klamby.com>
2013-11-20 12:58:24 +0100

    Some html helpers

---
+++ NumberGuessingGame/Helpers/GameTranslationHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web.Mvc;
+using NumberGuessingGame.Models;
+
+namespace NumberGuessingGame.Helpers
+{
+    public static class GameTranslationHelper
+    {
+        public static string NumberToSwedish(this HtmlHelper helper, int number)
+        {
+            string[] swedish = { "Inga", "Första", "Andra", "Tredje", "Fjärde", "Femte", "Sjätte", "Sjunde" };
+            return swedish[number];
+        }
+
+        public static MvcHtmlString GuessToText(this HtmlHelper helper, GuessedNumber guess)
+        {
+            var outcome = new OrderedDictionary();
+            outcome.Add(Outcome.High, "&darr;");
+            outcome.Add(Outcome.Low, "&uarr;");
+
+            return MvcHtmlString.Create(String.Format("{0}, {1}", guess.Number, outcome[guess.Outcome]));
+        }
+    }
+}
